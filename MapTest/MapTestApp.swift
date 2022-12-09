43bc1f6c0a1e52e996334102be7ad9be886c52f7
moritamu <moris@r.sannet.ne.jp>
2022-12-09 14:13:55 +0900

    Initial Commit

---
+++ MapTest/MapTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapTestApp.swift
+//  MapTest
+//
+//  Created by 森田晋 on 2022/12/09.
+//
+
+import SwiftUI
+
+@main
+struct MapTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
